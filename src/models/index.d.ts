f2061b66a27cc3ef57f111b0bc834a6243e8d816
Wei He <git@weispot.com>
2020-11-09 23:06:11 -0500

    reproduce: set up env for 6363

---
+++ src/models/index.d.ts
@@ -0,0 +1,12 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+export declare class Todo {
+  readonly id: string;
+  readonly name: string;
+  constructor(init: ModelInit<Todo>);
+  static copyOf(source: Todo, mutator: (draft: MutableModel<Todo>) => MutableModel<Todo> | void): Todo;
+}
---
+++ src/models/schema.js
@@ -0,0 +1,34 @@
+export const schema = {
+    "models": {
+        "Todo": {
+            "name": "Todo",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "name": {
+                    "name": "name",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Todos",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "adb94ffe6a11d221f9e2d17e69b09280"
+};
